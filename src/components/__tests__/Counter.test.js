4d57511891a30127865dbd2e3757602273e6141f
David Kuseh <davegru203@gmail.com>
2019-10-03 15:06:12 +0000

    added some tests for Counter.js

--- src/components/__tests__/Counter.test.js
+++ src/components/__tests__/Counter.test.js
@@ -4,10 +4,11 @@ import * as rtl from '@testing-library/react';
 import Counter from '../Counter';
 
 let tools;
+const countLimit= 5;
 
 beforeEach(() => {
   rtl.cleanup();
-  tools = rtl.render(<Counter user='Peter' />);
+  tools = rtl.render(<Counter user='Peter'  />);
 });
 
 describe('Counter component', () => {
@@ -39,18 +40,49 @@ describe('Counter component', () => {
 
   it('can decrement the count by one by clicking decrement', () => {
     // implement
+    const decButton = tools.queryByTestId('decButton');
+
+    rtl.fireEvent.click(decButton);
+    expect(tools.queryByText(/0/)).not.toBeInTheDocument();
+    expect(tools.queryByText(/-1/)).toBeInTheDocument();
+
+    rtl.fireEvent.click(decButton);
+    expect(tools.queryByText(/-1/)).not.toBeInTheDocument();
+    expect(tools.queryByText(/-2/)).toBeInTheDocument();
+
   });
 
-  it('can reset the count clicking rest', () => {
+  it('can reset the count clicking reset', () => {
     // implement
+    const resetButton = tools.queryByTestId('resetButton');
+
+    rtl.fireEvent.click(resetButton);
+    expect(tools.queryByText(/0/)).toBeInTheDocument();
+
   });
 
   it('prevents the count from going over an upper limit', () => {
     // implement
+    const incButton = tools.queryByTestId('incButton');
+
+    for(let i = 0; i < countLimit; i++) {
+      rtl.fireEvent.click(incButton);
+    }
+    expect(tools.queryByText(/6/)).not.toBeInTheDocument();
+    expect(tools.queryByText(/5/)).toBeInTheDocument();
+
+
   });
 
   it('prevents the count from going under a lower limit', () => {
     // implement
+    const decButton = tools.queryByTestId('decButton');
+
+    for(let i = 0; i < 10; i++) {
+      rtl.fireEvent.click(decButton);
+    }
+    expect(tools.queryByText(/16/)).not.toBeInTheDocument();
+    expect(tools.queryByText(/5/)).toBeInTheDocument();
   });
 
   it('shows a warning once we hit the upper limit of the counter', () => {
